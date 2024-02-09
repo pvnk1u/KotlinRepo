2631f1dbb1fbcb8ae4da4d89bb75d9032c2a8768
pvnk1u <pvnk1u@gmail.com>
2024-02-09 23:57:03 +0800

    合并类型检查和转换

---
+++ src/main/kotlin/chapter2/Expr.kt
@@ -0,0 +1,27 @@
+package chapter2
+
+import java.lang.IllegalArgumentException
+
+interface Expr
+
+//  简单的值对象类，只有一个属性value，实现了Expr接口
+class Num(val value:Int) : Expr
+
+// Sum运算的实参可以是任何Expr:Num或者另一个Sum
+class Sum(val left:Expr,val right:Expr) : Expr
+
+
+fun eval(e:Expr):Int{
+    if (e is Num){
+        val n = e as Num
+        return n.value
+    }
+    if (e is Sum){
+        return eval(e.right) + eval(e.left)
+    }
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun main(args: Array<String>){
+    println(eval(Sum(Sum(Num(1),Num(2)),Num(4))))
+}
