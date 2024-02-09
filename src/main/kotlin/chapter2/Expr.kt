f063fe40cead816a4451666227493d2a8defee4b
pvnk1u <pvnk1u@gmail.com>
2024-02-10 00:58:58 +0800

    is Check code and note

--- src/main/kotlin/chapter2/Expr.kt
+++ src/main/kotlin/chapter2/Expr.kt
@@ -13,10 +13,12 @@ class Sum(val left:Expr,val right:Expr) : Expr
 
 fun eval(e:Expr):Int{
     if (e is Num){
+        // 显式地转换成类型Num是多余的
         val n = e as Num
         return n.value
     }
     if (e is Sum){
+        // 变量e被智能地转换了类型
         return eval(e.right) + eval(e.left)
     }
     throw IllegalArgumentException("Unknown expression")
