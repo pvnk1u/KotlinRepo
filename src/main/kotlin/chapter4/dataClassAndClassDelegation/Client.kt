1989e60a093cffc8df388335052dba7e15a020f0
pvnk1u <pvnk1u@gmail.com>
2024-02-20 00:05:20 +0800

    commit equals code and note

--- src/main/kotlin/chapter4/dataClassAndClassDelegation/Client.kt
+++ src/main/kotlin/chapter4/dataClassAndClassDelegation/Client.kt
@@ -3,9 +3,20 @@ package chapter4.dataClassAndClassDelegation
 class Client(val name:String,val postalCode: Int) {
 
     override fun toString() =  "Client(name=$name,postalCode=$postalCode)"
+
+    // Any是java.lang.Object的模拟：Kotlin中所有类的父类。可空类型Any? 意味着other是可以为空的
+    override fun equals(other: Any?): Boolean {
+        // 检查other是不是一个Client
+        if (other == null || other !is Client)
+            return false
+        return name == other.name && postalCode == other.postalCode
+    }
 }
 
 fun main(args:Array<String>){
     val client1 = Client("Alice",342562)
     println(client1)
+    val client2 = Client("Alice",342562)
+    println(client1 == client2)
+    // 重写equals方法前是false
 }
